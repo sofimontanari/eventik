dccd2b2f4c32dc36e8c94dda2f48d405deb87bc1
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-14 19:08:55 -0300

    controllers events, estimations, users y sus vistas

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,34 @@
 class UsersController < ApplicationController
   before_action :set_supplier, only: %i[show]
   skip_before_action :authenticate_user!, only: %i[show index]
+
   def index
-    @users = User.where(supplier: true)
+
+    if params[:event]
+      @event = Event.find(params[:event])
+      @name = @event.event_type.name
+      @suppliers = []
+      User.where(supplier: true).each do |supplier|
+        supplier.event_types.each do |event_type|
+          if event_type.name == @name
+            @suppliers.push(supplier)
+          end
+        end
+      end
+
+    else
+      @suppliers = User.where(supplier: true)
+    end
   end
 
   def show
+    @event = Event.find(params[:event])
+    @estimation = Estimation.new
   end
 
   private
 
   def set_supplier
-    @user = User.find(params[:id])
+    @supplier = User.find(params[:id])
   end
 end
