362df72d7a25168bf5073983d000a31b5017d681
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-14 16:10:51 -0300

    controlador events y pages

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,32 @@
 class EventsController < ApplicationController
-  
+
+  def index
+   
+  end
+
+  def new
+    @event_type_id = EventType.find_by(name: params[:event_type]).id
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(set_event_params)
+    @event.user = current_user
+    @event.status = 'En proceso'
+    @event.event_type_id = params[:event][:event_type_id].to_i
+
+    if @event.save
+      redirect_to root_path, notice: "Creaste tu evento, ahora podés buscar servicios"
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private
+
+  def set_event_params
+    params.require(:event).permit(:name, :date, :address)
+    # agregar fotos
+  end
 end
