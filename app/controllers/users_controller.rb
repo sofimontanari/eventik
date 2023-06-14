369fe72a332ac727d7153d4fb14a928f7bada656
david varon <varondavid20@gmail.com>
2023-06-14 12:00:32 -0500

    controller de user hecho

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_supplier, only: %i[show]
+  skip_before_action :authenticate_user!, only: %i[show index]
   def index
     @users = User.where(supplier: true)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_supplier
+    @user = User.find(params[:id])
   end
 end
