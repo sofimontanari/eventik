dd913fd42baacdeaeaab162860a5c41c1155c6f1
bertin traverso <bertin_traverso@hotmail.com>
2023-06-15 14:52:10 -0500

    homepage

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -1,5 +1,5 @@
 class EventType < ApplicationRecord
-  TYPES = ["Cumpleaños Infantiles", "Cumpleaños Adultos", "Despedidas",  "Casamientos", "Bautismos", "Baby Showers", "Empresariales", "15 Años", "Personalizados" ]
+  TYPES = ["Cumpleaños Infantiles", "Cumpleaños Adultos", "Despedidas", "Casamientos", "Bautismos", "Baby Showers", "Empresariales", "15 Años", "Personalizados" ]
   belongs_to :user
   has_many :events
   validates_presence_of :name
