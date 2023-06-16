80f639208b9d440d9a0028ef44d6d510b087d155
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-16 17:34:09 -0300

    cambios en la fecha del formularios de cotizaciones

--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -1,7 +1,7 @@
 class EventType < ApplicationRecord
   TYPES = ["Cumpleaños Infantiles", "Cumpleaños Adultos", "Despedidas", "Casamientos", "Bautismos", "Baby Showers", "Empresariales", "15 Años", "Personalizados" ]
   belongs_to :user
-  has_many :events
+  has_many :events, dependent: :destroy
   validates_presence_of :name
   validates_inclusion_of :name, in: TYPES
 end
