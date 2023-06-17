b00700c04ba76da5b7f6f0d38ad2623c55ae0f6e
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-17 16:58:19 -0300

    agregué los iconos, actualicé las variables para tomar las reviews en el controller de pages, modifiqué toda la home page y las variables de bootsrap así como los colores

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[home event_types]
 
   def home
+    @reviews = Review.all
+    @estimations = Estimation.all
+    @events = Event.all
   end
 
   def event_types
 
   end
 end
+
