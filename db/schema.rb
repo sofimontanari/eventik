431325fe4f71f8e12912c4cae4ab78f9093df4a2
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-06-19 09:11:27 -0300

    chat finalizado

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_165124) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_111453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_165124) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "estimations", force: :cascade do |t|
@@ -77,6 +83,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_165124) do
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
     t.float "rating"
@@ -109,6 +125,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_165124) do
     t.text "includes"
     t.text "benefits"
     t.text "notes"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -120,5 +137,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_165124) do
   add_foreign_key "event_types", "users"
   add_foreign_key "events", "event_types"
   add_foreign_key "events", "users"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "estimations"
 end
