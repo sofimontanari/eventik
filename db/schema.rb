38b363d20709d49cd66ec9820a29bbab4b2dad08
Christopher Figueroa <figueroa.chris@hotmail.com>
2023-06-20 14:42:26 -0400

    config mapping 20%

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_202212) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_20_164745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -129,6 +129,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_19_202212) do
     t.text "benefits"
     t.text "notes"
     t.string "nickname"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
