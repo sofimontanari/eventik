845785cc6ab450f31a6ec1d51fd6654e6277121b
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-07-21 11:08:55 -0300

    stimulus controler para editar el formulario de perfil de usuario

--- app/javascript/controllers/edit_form_controller.js
+++ app/javascript/controllers/edit_form_controller.js
@@ -2,7 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="controller"
 export default class extends Controller {
+
+  static targets = ["passwords", "currentpass"]
+
   connect() {
-    console.log(this.element)
+    console.log(this.element);
+  }
+
+  showPassword(event){
+    const checkpass = event.target.checked;
+    const passwords = this.passwordsTargets;
+    const currentpass = this.currentpassTarget;
+
+    if (checkpass) {
+      passwords.forEach((password) => {password.classList.remove('hidden')});
+      currentpass.classList.add('hidden');
+    } else {
+      passwords.forEach((password) => {password.classList.add('hidden')});
+      currentpass.classList.remove('hidden');
+    }
   }
 }
