548b175541b4a3a626b1b582787c5f46ab632029
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-07-21 13:26:53 -0300

    controler de stimulus para actualizar la contraseña

--- app/javascript/controllers/edit_form_controller.js
+++ app/javascript/controllers/edit_form_controller.js
@@ -3,23 +3,28 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="controller"
 export default class extends Controller {
 
-  static targets = ["passwords", "currentpass"]
+  static targets = ["passwords", "currentpass", "label"]
 
   connect() {
     console.log(this.element);
+    console.log(this.labelTarget);
   }
 
   showPassword(event){
     const checkpass = event.target.checked;
     const passwords = this.passwordsTargets;
     const currentpass = this.currentpassTarget;
+    const hint = this.currentpassTarget.nextElementSibling;
+    const label = this.labelTarget;
 
     if (checkpass) {
       passwords.forEach((password) => {password.classList.remove('hidden')});
-      currentpass.classList.add('hidden');
+      label.innerHTML = 'Confirmar contraseña*';
+      hint.textContent = "Confrimá tu nueva contraseña";
     } else {
       passwords.forEach((password) => {password.classList.add('hidden')});
-      currentpass.classList.remove('hidden');
+      label.innerHTML = 'Contraseña*';
+      hint.textContent = "Ingresá tu contraseña para confirmar los cambios";
     }
   }
 }
